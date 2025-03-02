ae8d59c2b2be3852a759a4fabf01f10b4b630b8f
Eduardo <wledu63@gmail.com>
2025-03-02 04:21:09 -0300

    fix import issue

--- memory/src/cache_ttl_control.rs
+++ memory/src/cache_ttl_control.rs
@@ -56,7 +56,7 @@ impl CacheTTLControl {
 
 #[cfg(test)]
 mod test {
-    use std::{sync::Arc, thread::{self, sleep}, time::Duration};
+    use std::{thread::sleep, time::Duration as TDuration};
 
     use chrono::{DateTime, Duration, Local};
 
@@ -69,7 +69,7 @@ mod test {
 
         future.timestamp()
     }
-    
+
     fn future_point_in_milis(milis: i64) -> i64 {
         let now: DateTime<Local> = Local::now();
         let ttl = Duration::milliseconds(milis);
@@ -92,20 +92,18 @@ mod test {
 
     #[tokio::test]
     async fn test_cleanup_expired() {
-        
         let _200ms = future_point_in_milis(200);
         let _300ms = future_point_in_milis(300);
         let _10sec = future_point_in_seconds(1);
-        
+
         let ctc = CacheTTLControl::new();
         ctc.set(_200ms, "value200ms".to_owned()).await;
         ctc.set(_300ms, "value300ms".to_owned()).await;
         ctc.set(_10sec, "value1s".to_owned()).await;
-        
-        sleep(Duration::from_secs(1));
-        
+
+        sleep(TDuration::from_secs(1));
+
         let expired_keys = ctc.cleanup_expired().await;
         assert_ne!(expired_keys, None, "Should have a len of 2 expired keys");
-        assert
     }
 }
